6c385ac62a6f6baf80a522749f84223cc8260e6a
Dana Huang <201911863@feualabang.edu.ph>
2021-12-06 10:29:15 +0800

    removed comments

--- profile.php
+++ profile.php
@@ -146,66 +146,16 @@
                                 <br><br>
                             </ul>
                         </div>
-					<!-- </div>
-					<div class="row mb-5 pb-5">
-						<div class="col-xl-8 col-lg-7">
-							Video description
-							<div class="tm-video-description-box">
-								<p>Account Information</p>
-							</div>							
-						</div>
-						
-					<div class="row pt-4 pb-5">
-						hatdog
-					</div> -->
+					
 				</main>
 
-                <!-- COMMENT OUT KO MUNA TO -->
-				<!-- Subscribe form and footer links -->
-                <!-- <div class="row mt-5 pt-3">
-                    <div class="col-xl-6 col-lg-12 mb-4">
-                        <div class="tm-bg-gray p-5 h-100">
-                            <h3 class="tm-text-primary mb-3">Do you want to get our latest updates?</h3>
-                            <p class="mb-5">Please subscribe our newsletter for upcoming new videos and latest information about our
-                                work. Thank you.</p>
-                            <form action="" method="GET" class="tm-subscribe-form">
-                                <input type="text" name="email" placeholder="Your Email..." required>
-                                <button type="submit" class="btn rounded-0 btn-primary tm-btn-small">Subscribe</button>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 mb-4">
-                        <div class="p-5 tm-bg-gray">
-                            <h3 class="tm-text-primary mb-4">Quick Links</h3>
-                            <ul class="list-unstyled tm-footer-links">
-                                <li><a href="#">Duis bibendum</a></li>
-                                <li><a href="#">Purus non dignissim</a></li>
-                                <li><a href="#">Sapien metus gravida</a></li>
-                                <li><a href="#">Eget consequat</a></li>
-                                <li><a href="#">Praesent eu pulvinar</a></li>
-                            </ul>    
-                        </div>                        
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 mb-4">
-                        <div class="p-5 tm-bg-gray h-100">
-                            <h3 class="tm-text-primary mb-4">Our Pages</h3>
-                            <ul class="list-unstyled tm-footer-links">
-                                <li><a href="#">Our Videos</a></li>
-                                <li><a href="#">License Terms</a></li>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Contact</a></li>
-                                <li><a href="#">Privacy Policies</a></li>
-                            </ul>
-                        </div>                        
-                    </div>
-                </div> row -->
 
                 <footer class="row pt-5">
                     <div class="col-12">
                         <p class="text-right">Copyright 2020 The Duckmovies Company</p>
                     </div>
                 </footer>
-			</div> <!-- .tm-content-container -->
+			</div>
 		</div>
 	</div>
 
